3a0276f52d38073274e6910cf200f9ee7d64081d
Wamahiu-hub <josephkwamahiu@gmail.com>
2025-01-20 07:01:08 +0300

    Add files via upload
    These are some of my daily exercises

---
+++ index.js
@@ -0,0 +1,111 @@
+
+//let age = 25;
+//let price= 10.99;
+//let gpa = 2.1;
+
+//console.log(typeof age)
+//console.log(`you are ${age} years old`);
+//console.log(`the price is $${price}`);
+//console.log(`the gpa is ${gpa}`);
+
+//let firstName ="Bro"
+//let FavoriteFood ="Pizza"
+//let email="Bro@gmail.com"
+
+//console.log(typeof firstName);
+//console.log(`My Name is ${firstName}`);
+//console.log(`my favorite food is ${FavoriteFood}`);
+//console.log(`my email is ${email}`);
+
+//let online=true
+//let ForSale=true
+//let isStudent=true
+
+//console.log(typeof online)
+//console.log(`she is online: ${online}`)
+//console.log(`this car is for sale: ${ForSale}`)
+//console.log(`she is enrolled:${isStudent}`)
+
+//let fullName = "Bro Code";
+//let age = 25;
+//let IsStudent = true;
+
+//document.getElementById("p1").textContent =  `My name is ${fullName}`;
+//document.getElementById("p2").textContent =  `I am ${age} years old`;
+//document.getElementById("p3").textContent =  ` I am a student ${IsStudent}`
+
+//let students=30;
+//students=students+1
+//students=students-1
+//students=students*2
+//students=students/2
+//students=students**2
+//console.log(students);
+
+//let age= window.prompt ("how old are you")
+//age= Number(age)
+//age+=1
+
+//console.log(age)
+
+//const PI = 3.14259
+//let radius;
+//let Circumference;
+
+//document.getElementById("MySubmit").onclick = function () {
+    //radius= document.getElementById("MyText").value;
+    //radius = Number (radius);
+    //Circumference = 2 * PI * radius;
+    //document.getElementById("myH3").textContent = Circumference + "cm"
+//}
+
+//const decreaseBtn = document.getElementById("decreaseBtn");
+//const resetBtn = document.getElementById("resetBtn");
+//const increaseBtn = document.getElementById("increaseBtn");
+//const countLabel = document.getElementById("countLabel");
+//
+//increaseBtn.onclick=function(){
+//    count++;
+//    countLabel.textContent=count;
+//}
+//decreaseBtn.onclick=function(){
+ //   count --;
+   // countLabel.textContent=count;
+//}
+//resetBtn.onclick=function(){
+    //count=0
+    //countLabel.textContent=count
+//}
+
+//const myTEXT1 = document.getElementById("myTEXT1");
+//const Mysubmit1 = document.getElementById("Mysubmit1");
+//const resultElement = document.getElementById("resultElement");
+
+//let age;
+
+//Mysubmit1.onclick = function(){
+   // age=myTEXT1.value;
+    //age=Number(age);
+
+   // if (age >= 100) {
+    //    resultElement.textContent=`you are TOO old to enter this site`;
+   // }
+   // else if (age == 0){
+    //    resultElement.textContent=`You must have been born on a certain date`;
+   // }
+    //else if (age>18){
+   //     resultElement.textContent=`You  are old enough to enter this site`;
+  //  }
+  //  else if (age<0){
+  //      resultElement.textContent=`you cant have negative years`;
+  //  }
+   // else {
+   //     resultElement.textContent=`you must be 18+ to enter this site`;
+ //   }
+//}
+
+
+let userName= "BroCode  "
+
+
+console.log(userName.toUpperCa  se());
